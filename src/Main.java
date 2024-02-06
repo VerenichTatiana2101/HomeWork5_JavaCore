a14b607d17347ebe7db88bbf937012a17d8d4a3f
Verenich Tatiana <verenik91@gmail.com>
2024-02-06 20:47:00 +0300

    Домашняя работа 5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 public class Main {
-
     public static void main(String[] args) {
         String sourceDirectory = ".";
         String backupDirectory = "./backup";
@@ -16,6 +14,12 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Ошибка при создании копии: " + e.getMessage());
         }
+
+
+        int[] array = {1, 2, 0, 3, 2, 1, 0, 3, 1};
+        String fileName = "file.txt";
+
+        packNum(fileName, array);
     }
 
     /**
@@ -44,9 +48,29 @@ public class Main {
                 Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
             }
         }
-
-
     }
+
+    /**
+     * числа в исходном массиве из 9 элементов имеют диапазон[0, 3],
+     * и представляют собой, например, состояния ячеек поля для игры в крестикинолики,
+     * где 0 – это пустое поле, 1 – это поле с крестиком, 2 – это поле с ноликом,3 – резервное значение.
+     * Такое предположение позволит хранить в одном числе типа int всё поле 3х3.
+     * Записать в файл 9 значений так, чтобы они заняли три байта
+     * @param file для хранения битовых значений
+     * @param arr входящий массив
+     */
+    private static void packNum(String file, int[] arr){
+        try (DataOutputStream input = new DataOutputStream(new FileOutputStream(file))) {
+            for (int value : arr) {
+                input.writeByte(value);
+            }
+            System.out.println("Запись выполнена.");
+        } catch (IOException e) {
+            System.err.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+    }
+
+
 }
 
 
